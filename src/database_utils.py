cf9a291fc7644811fb44dd8189d840110fb1f992
Khalid Tarek <khalid.t.mostafa@gmail.com>
2024-03-07 19:08:30 +0200

    installed db and ibm_db driver

--- src/database_utils.py
+++ src/database_utils.py
@@ -2,10 +2,29 @@ import json
 import os
 os.add_dll_directory(f"{os.getcwd()}/env/Lib/site-packages/clidriver/bin/../bin")
 import ibm_db
-import ibm_db_sa
-from sqlalchemy import create_engine
 
-f = open("config.json")
-data = json.load(f)
-f.close()
+def extract_credentials():
+    f = open("config.json")
+    db2 = json.load(f)["db2"]
+    f.close()
 
+    return db2
+
+db2 = extract_credentials()
+
+connection_string = (
+    f"DATABASE={db2['database_name']};"
+    f"HOSTNAME={db2['database_hostname']};"
+    f"PORT={db2['database_port']};"
+    f"PROTOCOL=TCPIP;"
+    f"UID={db2['database_username']};"
+    f"PWD={db2['database_password']};"
+)
+
+#Make sure to close the db connection after you're done
+def get_ibm_db_connection():
+    try:
+        conn = ibm_db.connect(connection_string, '', '')
+        print("Connection established successfully.")
+    except Exception as e:
+        print(f"Error: {e}")
