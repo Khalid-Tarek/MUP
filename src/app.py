373ce53e4f564a771793a021dadff8eeda6f0adf
Khalid Tarek <khalid.t.mostafa@gmail.com>
2024-03-08 01:05:37 +0200

    database connection successful

--- src/app.py
+++ src/app.py
@@ -1,29 +1,28 @@
 from flask import Flask, render_template
 from database_utils import *
 app = Flask(__name__, template_folder="templates")
- 
-MUP = [
-    [0, "Khalid Tarek", "2/1/2023", "1/3/2024"],
-    [1, "Hassan Mohammedi", "2/1/2023", "1/3/2024"],
-    [2, "Alaa Fathi", "2/1/2023", "1/3/2024"],
-    [3, "Basel Elsayed", "2/1/2023", "1/3/2024"],
-]
 
 conn = get_ibm_db_connection()
 
 # Get table headers
 stmt = ibm_db.exec_immediate(conn, "SELECT colname FROM syscat.columns WHERE TABNAME = 'TEST'")
-try:
-    MUP_header = sum(parse_db2_statement(stmt), tuple()) # I do this to flatten the 2D result list
-    print(f"headers = {MUP_header}")
-except Exception as e:
-    print(f"Error: {e}")
-    ibm_db.close(conn)
+soldiers_table_headers = sum(parse_db2_statement(stmt), tuple()) # I do this to flatten the 2D result list
+print(f"soldiers_table_headers = {soldiers_table_headers}")
 
+# Get table content
+stmt = ibm_db.exec_immediate(conn, "SELECT * FROM TEST")
+soldiers_table_rows = parse_db2_statement(stmt)
+print(f"soldiers_table_rows = {soldiers_table_rows}")
+
+ibm_db.close(conn)
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html", MUP_header=MUP_header, MUP=MUP)
+    return render_template(
+        "index.html", 
+        soldiers_table_headers = soldiers_table_headers, 
+        soldiers_table_rows = soldiers_table_rows
+    )
 
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
